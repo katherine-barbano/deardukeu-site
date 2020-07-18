80c03fd22b83f10dba1d313a9f58c7bb59e5d772
Anna Mollard <acmollard@gmail.com>
2020-07-18 18:51:17 -0400

    ig info on site

--- dearduke-site/routes/index.js
+++ dearduke-site/routes/index.js
@@ -2,8 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  const fs = require('fs')
+  captions = []
+  fs.readFile(process.cwd() + '/data/deardukeu.json', 'utf8', (err, fileContents) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    try {
+      const data = JSON.parse(fileContents);
+      for(i=0; i<data.length; i++){
+        captions.push(data[i].node.edge_media_to_caption.edges[0].node.text);
+      }
+      read_ig_data(captions)
+    } catch(err) {
+      console.error(err)
+    }
+  })
+  function read_ig_data(captions) {
+    router.get('/', function(req, res, next) {
+      res.render('index', { text: captions });
+    });
+  }
+// router.get('/', function(req, res, next) {
+//   res.render('index', { text: "hi" });
+// });
 
 module.exports = router;
