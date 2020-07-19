5f38a4f02c07d78ffc181b203fb857e317183a83
Laura Li <li.l@salesforce.com>
2020-07-18 20:26:47 -0500

    more changes to website lol

--- dearduke-site/routes/index.js
+++ dearduke-site/routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+function display() {
+  console.log("hello");
+}
+/* $('.box').on('click', function() {
+  var $element = $(this);
+  
+  var $openElement = $('<div class="absoluteBox"></div>');
+  
+  $openElement.css('left', $element.position().left);
+  $openElement.css('right', $element.width());
+      
+  $element.html($openElement);
+  
+  $openElement.animate({left : 0, width : $('.wrapper').width()}, 500);
+ 
+}); */
+
 module.exports = router;
