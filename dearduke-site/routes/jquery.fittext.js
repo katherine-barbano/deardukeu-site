608e4e19f35f125c9c35a026203ad7fd5142e45f
Laura Li <li.l@salesforce.com>
2020-07-18 23:38:29 -0500

    start of fittext

---
+++ dearduke-site/routes/jquery.fittext.js
@@ -0,0 +1,50 @@
+/*global jQuery */
+/*!
+* FitText.js 1.2
+*
+* Copyright 2011, Dave Rupert http://daverupert.com
+* Released under the WTFPL license
+* http://sam.zoy.org/wtfpl/
+*
+* Date: Thu May 05 14:23:00 2011 -0600
+*/
+app.get('/routes/jquery.fittext.js', function(req, res) {
+  res.sendFile(path.join(__dirname + '/jquery.fittext.js'));
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/views/index.ejs'));
+});
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
